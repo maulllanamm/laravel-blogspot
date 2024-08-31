27dd0904f2e6a059d0282dc92535605668e8571e
maulllanamm <maulllanamuhammad@gmail.com>
2024-08-31 17:22:11 +0700

    add categories table

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,11 @@
     <main>
         <article class="py-8">
             <h2>{{$post['title']}}</h2>
-            <a href="/authors/{{$post->author->id}}">{{$post->author->name}} | {{ $post->created_at->diffForHumans() }}</a>
+            <h2>
+                By <a href="/authors/{{$post->author->username}}"> {{$post->author->name}}</a>
+                In <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+                | {{ $post->created_at->diffForHumans() }}
+            </h2>
             <p>{{$post['body']}}</p>
             <a href="/posts">&laquo; Back to post</a>
         </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,9 @@
                     <h2>{{$post['title']}}</h2>
                 </a>
                 <h2>
-                    By <a href="/authors/{{$post->author->id}}">{{$post->author->name}} | {{ $post->created_at->diffForHumans() }}</a>
+                    By <a href="/authors/{{$post->author->username}}"> {{$post->author->name}}</a>
+                    In <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+                     | {{ $post->created_at->diffForHumans() }}
                 </h2>
                 <p>{{Str::limit($post['body'], 150)}}</p>
                 <a href="/posts/{{$post['slug']}}">Read more &raquo;</a>
