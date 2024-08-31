5ab62e521528a4d3581be6698a9b3f5c9f519230
maulllanamm <maulllanamuhammad@gmail.com>
2024-08-31 14:28:23 +0700

    create model and factory Posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,9 @@
                 <a href="/posts/{{$post['slug']}}">
                     <h2>{{$post['title']}}</h2>
                 </a>
-                <a href="">{{$post['author']}} | 1 Juli 2024</a>
+                <h2>
+                    By <a href="">{{$post['author']}} | {{ $post->created_at->diffForHumans() }}</a>
+                </h2>
                 <p>{{Str::limit($post['body'], 150)}}</p>
                 <a href="/posts/{{$post['slug']}}">Read more &raquo;</a>
             </article>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-    return view('post', ['title' => 'Detail Post', 'post' => Post::find($slug)]);
+Route::get('/posts/{post:slug}', function (Post $post) {
+    return view('post', ['title' => 'Detail Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
