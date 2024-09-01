503452a52861944eb2b058dbc46ed6e743f3a262
maulllanamm <maulllanamuhammad@gmail.com>
2024-09-01 08:58:30 +0700

    fix card post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,12 +8,12 @@
                     <address class="flex items-center my-6 not-italic">
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
                             <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="{{$post->author->name}}">
-                            <div>
+                            <div class="flex flex-col">
                                 <a href="/authors/{{$post->author->username}}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white hover:underline">{{$post->author->username}}</a>
                                 <a href="/categories/{{$post->category->slug}}">
-                                   <span class="bg-{{$post->category->color}}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                   <p class="bg-{{$post->category->color}}-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                                         {{$post->category->name}}
-                                   </span>
+                                   </p>
                                 </a>
                                 <p class="text-base text-gray-500 dark:text-gray-400">{{$post->created_at->diffForHumans()}}</p>
                             </div>
