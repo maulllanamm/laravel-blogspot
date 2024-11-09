317e60cb2fced1c6c6979a38988624aff0739e2d
maulllanamm <maulllanamuhammad@gmail.com>
2024-11-09 11:56:54 +0700

    feat : resource & models

--- database/migrations/2024_11_09_043654_create_posts_table.php
+++ database/migrations/2024_11_09_043654_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('content');
-            $table->boolean('is_publish')->default(false);
+            $table->boolean('is_published')->default(false);
             $table->timestamps();
         });
     }
