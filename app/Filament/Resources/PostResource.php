090268f4cb5a645dd2835cb9619f9b27cf750772
maulllanamm <maulllanamuhammad@gmail.com>
2024-11-09 18:53:39 +0700

    feat : relation manager posts resource

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CategoryResource\RelationManagers\PostsRelationManager;
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
+use App\Filament\Resources\PostResource\RelationManagers\TagsRelationManager;
 use App\Models\Post;
 use Filament\Forms;
 use Filament\Forms\Components\Card;
@@ -81,7 +82,7 @@ class PostResource extends Resource
     public static function getRelations(): array
     {
         return [
-
+            TagsRelationManager::class
         ];
     }
 
---
+++ app/Filament/Resources/PostResource/RelationManagers/TagsRelationManager.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources\PostResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class TagsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'tags';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    TextInput::make('name')
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state)))
+                        ->required(),
+
+                    TextInput::make('slug')
+                ])
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                TextColumn::make('id')
+                    ->sortable(),
+                TextColumn::make('name')
+                    ->limit(50)
+                    ->sortable(),
+                TextColumn::make('slug')
+                    ->limit(50),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+                Tables\Actions\AttachAction::make(),
+                
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
